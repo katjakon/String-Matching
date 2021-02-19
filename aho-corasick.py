79b89db2768d56099ac540d18bd5452cdff80473
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-02-19 16:48:34 +0100

    Add failure function and pattern match function

--- aho-corasick.py
+++ aho-corasick.py
@@ -9,26 +9,79 @@ class AhoCorasick:
 
     def __init__(self, keywords):
         self.keywords = keywords
-        self.goto = dict()
+        self._goto = dict()
         self.output = dict()
+        self.fail = dict()
+        self.next = dict()
 
         self.__construct()
 
+
     def __construct(self):
+        self.__construct_goto()
+        self.__construct_fail()
+
+
+    def goto(self, state, char):
+        if state in self._goto:
+            if state == 0 and char not in self._goto[state]:
+                return 0
+            elif char in self._goto[state]:
+                return self._goto[state][char]
+        return False
+
+
+    def __construct_goto(self):
+        # Construct goto function and partial output function.
         newstate = 0
         for word in self.keywords:
             state = 0
             for char in word:
-                if state in self.goto and char in self.goto[state]:
-                    state = self.goto[state][char]
+                if state in self._goto and char in self._goto[state]:
+                    state = self.goto(state, char)
                 else:
                     newstate += 1
-                    self.goto.setdefault(state, dict())
-                    self.goto[state][char] = newstate
+                    self._goto.setdefault(state, dict())
+                    self._goto[state][char] = newstate
                     state = newstate
             self.output.setdefault(state, set()).add(word)
 
+    def __construct_fail(self):
+        chars = set()
+        for word in self.keywords:
+            word = set(word)
+            chars.update(word)
+        chars = list(chars)
+        # Construct failure function and final output function.
+        queue = []
+        for char in chars:
+            if self.goto(0, char) is not False and self.goto(0, char) != 0:
+                queue.append(self.goto(0, char))
+                self.fail[self.goto(0, char)] = 0
+        while queue:
+            state = queue.pop(0)
+            for char in chars:
+                if self.goto(state, char) is not False:
+                    out = self.goto(state, char)
+                    queue.append(out)
+                    curr_state = self.fail[state]
+                    while self.goto(curr_state, char) is False:
+                        curr_state = self.fail[curr_state]
+                    self.fail[out] = self.goto(curr_state, char)
+                    output = self.output.get(out, set())
+                    if self.fail[out] in self.output:
+                        self.output[out] = output.union(self.output[self.fail[out]])
+
+    def match_pattern(self, input_text, start=0):
+        state = 0
+        for i, char in enumerate(input_text):
+            while self.goto(state, char) is False:
+                state = self.fail[state]
+            state = self.goto(state, char)
+            if state in self.output:
+                for out in self.output[state]:
+                    print(out, start+i-len(out)+1)
+
 a = AhoCorasick(["he", "she", "his", "hers"])
-print(a.goto)
-print(a.output)
+print(a.match_pattern("123ushersgg", index=0))
 
