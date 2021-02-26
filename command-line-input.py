0863564e0c018bfa450d9111ea5f86024614df80
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-02-26 16:02:09 +0100

    Add method for matching in a file

--- command-line-input.py
+++ command-line-input.py
@@ -32,8 +32,27 @@ class Search:
             return True
         return False
 
-    def _match_in_file(self):
-        pass
+    def print_matches(self, match_dict):
+        for match in match_dict:
+            print("{}: {}".format(match,
+                                  ",".join([str(i) for i in match_dict[match]])))
+
+    def _match_in_file(self, match):
+        with open(self.text, encoding="utf-8") as file_in:
+            count = 1
+            at = 0
+            matches = dict()
+            for line in file_in:
+                if self.v:
+                    matches = match.match_pattern(line)
+                    if matches:
+                        print(count)
+                        self.print_matches(matches)
+                else:
+                    matches = match.match_pattern(line, start=at, matches=matches)
+                    at += len(line)
+                count += 1
+            self.print_matches(matches)
 
     def _match_in_dir(self):
         pass
@@ -42,9 +61,7 @@ class Search:
         if self.i:
             text = text.lower()
         matches = match.match_pattern(text, start=at)
-        for match in matches:
-            print("{}: {}".format(match,
-                                  ",".join([str(i) for i in matches[match]])))
+        self.print_matches(matches)
 
     def _pattern_from_file(self):
         pass
@@ -61,12 +78,12 @@ class Search:
             pattern = [keyword.lower() for keyword in pattern]
         match = StringMatching(algorithm=alg, keywords=pattern)
         if self.input_from_file:
-            self._match_in_file()
+            self._match_in_file(match)
         elif self.input_from_dir:
             self._match_in_dir()
         else:
             self._match_in_str(match, self.text)
 
 if __name__ == "__main__":
-    s = Search(["she", "he","hers", "his"], None, "ushershe", False, False, False)
+    s = Search(["she", "he","hers", "his"], None, "demo/demo1.txt", False, False, False)
     s.run()
