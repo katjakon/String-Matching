018e3614ff91ac7c7c1ab91e360dd5cbacdeca16
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-02-26 14:52:24 +0100

    Add main function for programm

--- command-line-input.py
+++ command-line-input.py
@@ -4,28 +4,21 @@ Created on Mon Feb 22 12:12:37 2021
 
 @author: HP I5
 """
+import string_matching
 
+class Search:
 
-class CommandLineInput:
-
-    COMMANDS = ("suche", "speicher", "demo")
-    OPTIONS = ("-i", "-n", "-v")
     INPUT_EXT = (".txt")
     PATTERN_EXT = (".json")
     DIRS = ("\\", "/")
 
-    def __init__(self, commands):
-        self.commands = commands
-        self.command = None
-        self.pattern = None
-        self.input_text = None
-        self.options = []
-
-    @property
-    def pattern_from_file(self):
-        if self.pattern.endswith(self.PATTERN_EXT):
-            return True
-        return False
+    def __init__(self, pattern, pattern_file, text, i, v, n):
+        self.pattern = pattern
+        self.pattern_file = pattern_file
+        self.text = text
+        self.i = i
+        self.v = v
+        self.n = n
 
     @property
     def input_from_file(self):
@@ -39,22 +32,15 @@ class CommandLineInput:
             return True
         return False
 
-    def parse_commands(self):
-        if self.commands[0] not in self.COMMANDS:
-            raise ValueError("Invalid command '{}'.".format(self.commands[0]))
-        self.command = self.commands[0]
-        for i in range(1, len(self.commands)):
-            if not self.commands[i].startswith("-"):
-                break
-            if self.commands[i] not in self.OPTIONS:
-                raise ValueError("Unknown option: {}".format(self.commands[i]))
-            self.options.append(self.commands[i])
-        self.pattern = self.commands[i]
-        self.input_text = self.commands[i+1]
+    def _match_in_file(self):
+        pass
 
-if __name__ == "__main__":
-    c = ["suche", "wedc.json", "wlek/"]
-    i = CommandLineInput(c)
-    i.parse_commands()
-    print(i.command)
-    print(i.pattern)
+    def _match_in_dir(self):
+        pass
+
+    def _pattern_from_file(self):
+        pass
+
+    def run(self):
+        if self.pattern:
+            
