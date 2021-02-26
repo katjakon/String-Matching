78e6d9a6cd09c1c820f412c5107e1d4df9b727f9
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-02-26 09:36:50 +0100

    Add more arguments for argparse

--- parse_input.py
+++ parse_input.py
@@ -6,28 +6,22 @@ Created on Wed Feb 24 14:54:52 2021
 """
 
 import argparse
-import re
 
 input_parser = argparse.ArgumentParser()
-subs = input_parser.add_subparsers()
-search_p = subs.add_parser("suche")
-save_p = subs.add_parser("speicher")
+subs = input_parser.add_subparsers(dest='command')
+search_p = subs.add_parser("search")
+save_p = subs.add_parser("save")
 demo_p = subs.add_parser("demo")
 
 
-
-def split_pattern(pattern_strings):
-    return re.split(r"(?!\\),", pattern_strings)
-
-print(split_pattern("hallo,hallo ich\,weiß"))
-
 # input_parser.add_argument("command", choices=["search", "save", "demo"])
-input_parser.add_argument("-i", action="store_true")
+input_parser.add_argument("-i", "-insensitive", action="store_true")
 search_p.add_argument("-n", "-naive",  action="store_true")
-input_parser.add_argument("-v", "-verbose",  action="store_true")
+input_parser.add_argument("-v", "-verbose", action="store_true")
 g = search_p.add_mutually_exclusive_group(required=True)
-g.add_argument("--pattern", nargs="+")
-g.add_argument("--pattern_file")
+g.add_argument("--pattern", type=lambda x: x.split(","))
+g.add_argument("--pattern_file", type=argparse.FileType('r', encoding='UTF-8'))
 search_p.add_argument("input_text")
 a =input_parser.parse_args()
 
+print(a)
