2605fc9c78c9ce1522207a3a4562c303bec6aeb8
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-03-01 20:22:53 +0100

    Add help messages for argparse

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ from user_commands import Search
 def main():
     parser = argparse.ArgumentParser()
     subs = parser.add_subparsers(dest='command')
-    search_p = subs.add_parser("search")
-    save_p = subs.add_parser("save")
-    demo_p = subs.add_parser("demo")
+    search_p = subs.add_parser("search", help="Search for keywords in a string, file or directory, type 'search -h' for more info")
+    save_p = subs.add_parser("save", help="Save a set of keywords to a file")
+    demo_p = subs.add_parser("demo", help="Get a demo with examples for input and output")
     search_p.add_argument("-n", "-naive",  action="store_true")
     search_p.add_argument("-i", "-insensitive", action="store_true")
     search_p.add_argument("-v", "-verbose", action="store_true")
