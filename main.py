27f328f2ce155571825b14445fdd819400188b5b
Katja Konermann <katja.konermann@uni-potsdam.de>
2021-03-06 13:15:07 +0100

    Clean up code

--- main.py
+++ main.py
@@ -1,43 +1,60 @@
 # -*- coding: utf-8 -*-
+# Katja Konermann
+# Matrikelnummer: 802658
 """
-Created on Wed Feb 24 14:54:52 2021
+Main file of the String Matching Programm.
 
-@author: HP I5
+Command line input is handled here by argparse.
 """
-
 import argparse
 
 from user_commands import Search
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="String matching programm")
     subs = parser.add_subparsers(dest='command')
     search_p = subs.add_parser("search",
                                help="Search for keywords in a string, file "
                                "or directory, type 'search -h' for more info")
     demo_p = subs.add_parser("demo",
-                             help="Get a demo with examples for input and output")
-    search_p.add_argument("-n", "-naive", action="store_true", help="Use naive matching algorithm")
-    search_p.add_argument("-i", "-insensitive", action="store_true", help="Match case insensitive")
-    search_p.add_argument("-v", "-verbose", action="store_true", help="Get verbose ouput when matching in files")
-    search_p.add_argument("input", help="Text to search in, can be a string, file or directory")
-    search_p.add_argument("pattern", nargs="+", help="Patterns to search for.")
+                             description="Get a demo with examples "
+                             "for input and output",
+                             help="Get a demo with examples "
+                             "for input and output")
+    search_p.add_argument("-n",
+                          "-naive",
+                          action="store_true",
+                          help="Use naive matching algorithm")
+    search_p.add_argument("-i",
+                          "-insensitive",
+                          action="store_true",
+                          help="Match case insensitive")
+    search_p.add_argument("-v",
+                          "-verbose",
+                          action="store_true",
+                          help="Get verbose ouput when matching in files")
+    search_p.add_argument("input",
+                          help="Text to search in, "
+                          "can be a string, file or directory")
+    search_p.add_argument("pattern",
+                          nargs="+",
+                          help="Patterns to search for.")
     args = parser.parse_args()
     if args.command == "search":
         try:
             search = Search(pattern=args.pattern,
                             input_text=args.input,
-                            i=args.i,
-                            v=args.v,
-                            n=args.n)
+                            insensitive=args.i,
+                            verbose=args.v,
+                            naive=args.n)
             try:
                 search.run()
-            except OSError as e:
-                print(e)
+            except OSError as error:
+                print(error)
                 search_p.print_help()
-        except ValueError as e:
-            print(e)
+        except ValueError as error:
+            print(error)
             search_p.print_help()
     elif args.command == "demo":
         Search.demo()
